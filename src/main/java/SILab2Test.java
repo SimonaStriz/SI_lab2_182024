d20c704821c3d24bcbb69da33a47ae2f1ae3139d
simonastriz <strizloska.129@gmail.com>
2020-06-13 18:11:04 +0200

    project added

---
+++ src/main/java/SILab2Test.java
@@ -0,0 +1,15 @@
+import jdk.internal.vm.annotation.ReservedStackAccess;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class SILab2Test {
+    private SILab2 lab2 = new SILab2();
+    private List<String> createList(String... elements){
+        return Arrays.asList(elements);
+    }
+    @Test
+
+}
